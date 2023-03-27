6e1c85174c6ab481c815a2317d28b324a62e2ac0
CarpMarcel02 <99967078+CarpMarcel02@users.noreply.github.com>
2023-03-27 11:37:33 +0300

    Delete InvalidaCatalogException.java

--- TemaSeminar5/InvalidaCatalogException.java
+++
@@ -1,8 +0,0 @@
-package org.Seminar5;
-
-class InvalidCatalogException extends Exception {
-    public InvalidCatalogException(Exception ex) {
-        super("Invalid catalog file.", ex);
-    }
-}
-
